102f37ea1996f60a2d18fefe147748a752191baa
Mani Jafari <manielli@gmail.com>
2018-11-09 18:23:44 -0800

    Created boxIt.js file. Primary structure of functions and their inputs were defined.

---
+++ boxit.js
@@ -0,0 +1,30 @@
+process.argv();
+
+var args = process.argv.slice(2);
+let str = [];
+
+function drawline (num){
+
+}
+
+function drawTopBorder (num) {
+
+}
+
+function drawMiddleBorder (num) {
+
+}
+
+function drawBottomBorder (num) {
+
+}
+
+function drawBarsAround (num) {
+
+}
+
+function boxIt (str){
+
+
+}
+
