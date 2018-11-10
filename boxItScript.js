97927972ff8afe81eb46e28b89d2bbdcfab99c5f
Mani Jafari <manielli@gmail.com>
2018-11-09 19:12:48 -0800

    completed the function drawLine.js

--- boxItScript.js
+++ boxItScript.js
@@ -1,13 +1,16 @@
-process.argv();
-
-var args = process.argv.slice(2);
-let str = [];
-
+//let args = process.argv[4];
+let num = process.argv.slice(2);
+let str = '';
 
 
-function drawline (num){
-
+function drawLine (num){
+    let str = '';
+    for (let i = 0; i < num; i++){      
+        str += '━━━━━';
+    }
+    return str;
 }
+console.log(drawLine(num));
 
 function drawTopBorder (num) {
 
