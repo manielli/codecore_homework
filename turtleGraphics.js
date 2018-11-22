e8f83c1d6d6634934b4d35499168ed24508402f3
Mani Jafari <manielli@gmail.com>
2018-11-21 18:01:19 -0800

    Second commit, created class Turtle

---
+++ turtleGraphics.js
@@ -0,0 +1,28 @@
+class Turtle {
+
+    coordinates (x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    forward (steps) {
+
+    }
+
+    right () {
+
+    }
+
+    left () {
+    
+    }
+
+    allPoints () {
+
+    }
+
+    print () {
+        
+    }
+
+}
