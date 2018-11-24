6f880c6c65f45bc5871da2ace25811101db2f51e
Mani Jafari <manielli@gmail.com>
2018-11-23 19:49:33 -0800

    Created two objects with • and spaces. Next step to assign the hasbeen array point to this object.

--- turtleGraphics.js
+++ turtleGraphics.js
@@ -2,54 +2,50 @@
 
 class Turtle {
 
-    
-    constructor (x,y) {
+
+    constructor(x, y) {
 
         this.x = x;
         this.y = y;
-        this.hasbeen = [[x,y]];
+        this.hasbeen = [[x, y]];
         this.direction = 'east';
     }
 
-    forward (steps) {
+    forward(steps) {
 
-        for ( let i = 0 ; i < steps ; i++) {
 
+        for (let i = 0; i < steps; i++) {
             switch (this.direction) {
                 case 'east':
                     this.x += 1;
                     // this.y = y;
-                    this.hasbeen.push([this.x,this.y]);
+                    this.hasbeen.push([this.x, this.y]);
                     this.direction = 'east';
                     break;
                 case 'north':
                     // this.x = x;
                     this.y += 1;
                     this.direction = 'north';
-                    this.hasbeen.push([this.x,this.y]);
+                    this.hasbeen.push([this.x, this.y]);
                     break;
                 case 'west':
                     this.x -= 1;
                     // this.y = y;
                     this.direction = 'west';
-                    this.hasbeen.push([this.x,this.y]);
-                  break;
+                    this.hasbeen.push([this.x, this.y]);
+                    break;
                 default:
                     // this.x = x;
                     this.y -= 1;
                     this.direction = 'south';
-                    this.hasbeen.push([this.x,this.y]);
-                  break;
-              }
-
+                    this.hasbeen.push([this.x, this.y]);
+                    break;
+            }
         }
         return this;
-
-        // for has been push into it all location that has been
-        // return `${this.x},${this.y}`;
-    
     }
-    left () {
+
+    left() {
         switch (this.direction) {
             case 'east':
                 this.direction = 'north'
@@ -59,16 +55,15 @@ class Turtle {
                 break;
             case 'west':
                 this.direction = 'south'
-              break;
+                break;
             default:
                 this.direction = 'east'
-              break;
-          }
-          return this;
+                break;
+        }
+        return this;
     }
 
-    right () {
-
+    right() {
         switch (this.direction) {
             case 'east':
                 this.direction = 'south'
@@ -78,30 +73,61 @@ class Turtle {
                 break;
             case 'west':
                 this.direction = 'north'
-              break;
+                break;
             default:
                 this.direction = 'east'
-              break;
-          }
-          return this;
+                break;
+        }
+        return this;
     }
-    
-    allPoints () {
-        console.log ('The turtle has been to: ', this.hasbeen);
+
+    allPoints() {
+        console.log('The turtle has been to: ', this.hasbeen);
     }
 
     print() {
-        arr = this.hasbeen;
+        let arr = this.hasbeen;
+        console.log('The length of hasbeen array is ', arr.length);
 
-        arr = arr.split();
-        console.log('The splitted array',arr);
-        // trickiest, I have to use the has been data and also x and y
-        // printing a big grid, using some loops
+        let arrX = [];
+        let arrY = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            arrX.push(parseInt(arr[i][0]));
+            arrY.push(parseInt(arr[i][1]));
+        }
+        let gridX = Math.max(...arrX) - Math.min(...arrX);
+        let gridY = Math.max(...arrY) - Math.min(...arrY);
+
+        console.log(gridX);
+        console.log(gridY);
+
+        let arrayDot = Array.from({length: gridX}).fill(' • ');
+        let output = {};
+        for(let key = 0; key < gridY; key ++){
+            output[`${key}`] = arrayDot;  
+        };
+
+        console.log(output);
+
+        for (let k = 0; k < gridY; k++) {
+            for (let j = 0; j < gridX; j++) {
+
+                    output[j][k] = '  '
+                
+            }
+        }
+
+        console.log(output);
+
+
+
     }
 
 };
 
-const tedTheTurtle = new Turtle(0,0);
-tedTheTurtle.left().right().forward(3).left().forward(3).allPoints().print();
+const tedTheTurtle = new Turtle(0, 0);
+tedTheTurtle.forward(6).left().forward(6).left().forward(6).left().forward(6).allPoints();
+tedTheTurtle.print();
 // const samTheTurtle = new Turtle(2,3);
 // const johnTheTurtle = new Turtle(5,5);
