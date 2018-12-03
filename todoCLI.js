f8edeba654ed43626411e696dcd8ce60ed0bb4dd
Mani Jafari <manielli@gmail.com>
2018-12-03 08:53:19 -0800

    Slight modifications to the todoCLI.js

--- todoCLI.js
+++ todoCLI.js
@@ -14,10 +14,10 @@ function toDoMenu (input){
 
     }   else if (input == "n") {
 
-        rl.question("\nWhat is the item you'd like to add", addItem);
+        rl.question("\nWhat is the item you'd like to add?\n>", addItem);
     }   else if (input == "cX") {
 
-        rl.close();
+        markCompleted(completedItem);
     }   else if (input == "dX") {
 
         rl.close();
@@ -39,20 +39,39 @@ Welcome to the To Do CLI:\n
 function printList (pathToList) {
     fs.readFile(pathToList, "utf8", (error, data) => {
         if (error) return console.error(error);
-        console.log(data);
+        
+        const regex = /,/gi;
+        console.log(`\n${data.replace(regex," ")}`);
+        rl.question(`\n\nWelcome to the To Do CLI:\n-------------------------\n(v) View    • (n) New   • (cX) Complete • (dX) Delete   • (q) quit\n\n>`, toDoMenu);
     });
-    rl.close();
-    // rl.question(`Welcome to the To Do CLI:\n\n--------------------------\n\n(v) View    • (n) New   • (cX) Complete • (dX) Delete   • (q) quit\n\n>`, toDoMenu);
+
 }
 
 function addItem (itemToAdd) {
 
     fs.readFile(".toDoList.csv", "utf8", (error, data) => {
         if (error) return console.error(error);
-        newListWithAddedItem = data.push(`${data.length},[ ],${itemToAdd}`);
-        
-        // fs.writeFile()
+        // console.log(data);
+        const splitData = data
+            .split("\n");
+        // console.log(splitData);
+
+        splitData.push(`${splitData.length},[ ],${itemToAdd}`);
+        const newList = splitData.join("\n");
+        fs.writeFile(oldList, newList, error => {
+        });
+
+        rl.question(`\n\nWelcome to the To Do CLI:\n-------------------------\n(v) View    • (n) New   • (cX) Complete • (dX) Delete   • (q) quit\n\n>`, toDoMenu);
     });
 
+}
+
+function markCompleted (completedItem) {
+    fs.readFile(".toDoList.csv", "utf8",(error, data) => {
+        if (error) return console.error(error);
+
+        
+
+    });
 }
 
