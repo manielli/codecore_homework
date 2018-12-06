7dfa9d06cfa956549dcbb790aad65829bc27fa8f
Mani Jafari <manielli@gmail.com>
2018-12-05 20:02:55 -0800

    new cohorts page works

--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,9 @@ const express = require("express");
 // const knex = require("../db/client");
 const router = express.Router();
 
+router.get("/new", (req, res) => {
+    res.render("posts/new");
+})
+
+
 module.exports = router;
