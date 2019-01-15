6eec31ea022e79250bcef7f528056689ce85511d
Mani Jafari <manielli@gmail.com>
2019-01-15 10:08:34 -0800

    Created post model, and post controller, configured routes.rb file, implemented CRUD operations

--- blog_on_rails/db/seeds.rb
+++ blog_on_rails/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+
+50.times do
+    created_at = Faker::Date.backward(365)
+
+    p = Post.create(
+        title: Faker::Lorem.sentence(5, true, 5),
+        body: Faker::Lorem.paragraph(5, true, 5),
+        created_at: created_at,
+        updated_at: created_at
+    )
+end
+
+posts = Post.all
+
+puts Cowsay.say("Generated #{posts.count} posts", :dragon)
